c43bf80ece3427f81803988c04ddfc9e42710158
baigali <baig.ali@gmail.com>
2024-02-17 20:44:47 -0800

    removed top lines

--- Exercise1 Problem1 Iris Classification.py
+++ Exercise1 Problem1 Iris Classification.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[110]:
-
-
 from sklearn.datasets import load_iris #our labels and targets comes from here
 from sklearn.model_selection import train_test_split # allows us to split training and testing data
 from sklearn.neighbors import KNeighborsClassifier
@@ -49,13 +43,7 @@ knn.fit(trainX,trainy)
 #make predictions based on training set
 knn.predict(testX)
 
+
 #lines 48 and 49 compare the actual values with our predictions
 #print(knn.predict(testX))
 #print(testy)
-
-
-# In[ ]:
-
-
-
-
